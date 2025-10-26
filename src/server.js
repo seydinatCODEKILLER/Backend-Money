ec34c2e2dd2d3cc7d885e85973d750e861c030a2
Ben Thiam <seydinat235@gmail.com>
2025-10-26 13:10:39 +0000

    correction de la variable d'ecoute du port

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 
-app.listen(env.PORT, env.HOST, () => {
+app.listen(env.PORT, () => {
   console.log(`✅ Serveur démarré sur : http://${env.HOST}:${env.PORT}`);
 });
